2e0fc325fe555d7486037cc723bbaca35b1ece80
sunnykumar77399 <bsssunny04@gmail.com>
2025-07-21 19:47:38 +0530

    Adde Change

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     photo: { type: String, required: true},
     firstName: { type: String },
     lastName: { type: String },
-    creditBalance: { type: Number, default: 0 },
+    creditBalance: { type: Number, default: 5 },
 
 })
 
